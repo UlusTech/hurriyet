661c810c3dc179bd28af2da039278d50418c4192
Who <w.who.am@proton.me>
2024-10-18 16:51:59 +0300

    switching to deno

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { TrayIcon } from "@tauri-apps/api/tray";
-import { Menu } from "@tauri-apps/api/menu";
-
-const menu = await Menu.new({
-  items: [
-    {
-      id: "quit",
-      text: "Quit",
-    },
-  ],
-});
-
-const options = {
-  menu,
-  menuOnLeftClick: true,
-};
-
-TrayIcon.new(options);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
